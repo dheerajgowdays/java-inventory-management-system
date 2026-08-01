bbb73e1f15a8e2d51a0105bc89d6cb62970d70f2
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-08-01 22:10:59 +0530

    feat: Uppdaing invoice repository

---
+++ src/com/dheeraj/inventory/repository/InvoiceRepository.java
@@ -0,0 +1,22 @@
+package repository;
+
+import model.Invoice;
+
+import java.util.HashMap;
+
+public class InvoiceRepository {
+
+    HashMap<Long, Invoice> invoices = new HashMap<>();
+
+    public void addInvoice(long id , Invoice invoice){
+        invoices.put(id,invoice);
+    }
+
+    public Invoice getById(long id){
+        if(invoices.containsKey(id)){
+            return invoices.get(id);
+        }
+        return null;
+    }
+    public int
+}
