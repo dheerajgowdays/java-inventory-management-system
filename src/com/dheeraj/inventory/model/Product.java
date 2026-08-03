a382bd45ffeb2ba4a01e020c8841963dddc63918
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-08-03 22:04:29 +0530

    feat: updating product model

--- src/com/dheeraj/inventory/model/Product.java
+++ src/com/dheeraj/inventory/model/Product.java
@@ -1,6 +1,8 @@
 package model;
 
-public class Product{
+import java.io.Serializable;
+
+public class Product implements Serializable {
 
     private final String productId;
     private String productName;
